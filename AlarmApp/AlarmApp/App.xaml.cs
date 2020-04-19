323c57688b8252689162adaaa21ee0e524404f07
Oludayo Alli <oludayo.alli@thecontentmannetwork.com>
2020-04-19 14:03:53 +0100

    Add project files.

---
+++ AlarmApp/AlarmApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("SEGOEUI.ttf", Alias = "RegularFont")]
+[assembly: ExportFont("SEGOEUIL.ttf", Alias = "LightFont")]
+[assembly: ExportFont("SEGUISB.ttf", Alias = "MediumFont")]
+namespace AlarmApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AlarmApp/AlarmApp/MainPage.xaml.cs
@@ -0,0 +1,125 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AlarmApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Device.StartTimer(TimeSpan.FromSeconds(1 / 60f), () =>
+            {
+                canvasView.InvalidateSurface();
+                return true;
+            });
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(1000), () =>
+            {
+                slider.TranslationX = -80;
+                slider.TranslateTo(80, 0, 800, Easing.Linear);
+
+                return true;
+            });
+        }
+
+        SKPath path = new SKPath();
+        float arcLength = 105;
+
+        DateTime alarmDate = GetNextAlarm();
+
+        private static DateTime GetNextAlarm()
+        {
+            DateTime today = DateTime.Today;
+            DateTime possibleDate = new DateTime(today.Year, today.Month, today.Day, 20, 15, 00);
+
+            if (DateTime.Now > possibleDate)
+                return possibleDate.AddDays(1);
+
+            return possibleDate;
+        }
+
+        private SKPaint GetPaintColor(SKPaintStyle style, string hexColor, float strokeWidth = 0, SKStrokeCap cap = SKStrokeCap.Round, bool IsAntialias = true)
+        {
+            return new SKPaint
+            {
+                Style = style,
+                StrokeWidth = strokeWidth,
+                Color = string.IsNullOrWhiteSpace(hexColor) ? SKColors.White : SKColor.Parse(hexColor),
+                StrokeCap = cap,
+                IsAntialias = IsAntialias
+            };
+        }
+
+        private void canvas_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
+        {
+            SKImageInfo info = e.Info;
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            SKPaint strokePaint = GetPaintColor(SKPaintStyle.Stroke, null, 10, SKStrokeCap.Square);
+            SKPaint dotPaint = GetPaintColor(SKPaintStyle.Fill, "#DE0469");
+            SKPaint hrPaint = GetPaintColor(SKPaintStyle.Stroke, "#262626", 4, SKStrokeCap.Square);
+            SKPaint minPaint = GetPaintColor(SKPaintStyle.Stroke, "#DE0469", 2, SKStrokeCap.Square);
+            SKPaint bgPaint = GetPaintColor(SKPaintStyle.Fill, "#FFFFFF");
+
+            canvas.Clear();
+
+            SKRect arcRect = new SKRect(10, 10, info.Width - 10, info.Height - 10);
+            SKRect bgRect = new SKRect(25, 25, info.Width - 25, info.Height - 25);
+            canvas.DrawOval(bgRect, bgPaint);
+
+            strokePaint.Shader = SKShader.CreateLinearGradient(
+                               new SKPoint(arcRect.Left, arcRect.Top),
+                               new SKPoint(arcRect.Right, arcRect.Bottom),
+                               new SKColor[] { SKColor.Parse("#DE0469"), SKColors.Transparent },
+                               new float[] { 0, 1 },
+                               SKShaderTileMode.Repeat);
+
+            path.ArcTo(arcRect, 45, arcLength, true);
+            canvas.DrawPath(path, strokePaint);
+
+            canvas.Translate(info.Width / 2, info.Height / 2);
+            canvas.Scale(info.Width / 200f);
+
+            canvas.Save();
+            canvas.RotateDegrees(240);
+            canvas.DrawCircle(0, -75, 2, dotPaint);
+            canvas.Restore();
+
+            DateTime dateTime = DateTime.Now;
+
+            //Draw hour hand
+            canvas.Save();
+            canvas.RotateDegrees(30 * dateTime.Hour + dateTime.Minute / 2f);
+            canvas.DrawLine(0, 5, 0, -60, hrPaint);
+            canvas.Restore();
+
+            //Draw minute hand
+            canvas.Save();
+            canvas.RotateDegrees(6 * dateTime.Minute + dateTime.Second / 10f);
+            canvas.DrawLine(0, 10, 0, -90, minPaint);
+            canvas.Restore();
+
+            canvas.DrawCircle(0, 0, 5, dotPaint);
+
+            secondsTxt.Text = dateTime.Second.ToString("00");
+            timeTxt.Text = dateTime.ToString("hh:mm");
+            periodTxt.Text = dateTime.Hour >= 12 ? "PM" : "AM";
+
+            var alarmDiff = alarmDate - dateTime;
+            alarmTxt.Text = $"{alarmDiff.Hours}h {alarmDiff.Minutes}m until next alarm";
+
+        }
+    }
+}
